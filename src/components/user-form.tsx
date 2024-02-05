099916e161730eade1206d0497e557d58518be39
Mark Lester Echavez <marklester.echavez@gmail.com>
2024-02-05 19:01:39 +0800

    refactor: setup shadcn form

--- src/components/user-form.tsx
+++ src/components/user-form.tsx
@@ -2,25 +2,114 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { FieldValues, useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z
+    .string({ required_error: "The name is required. " })
+    .min(3, { message: "The name must be at least 3 characters long." }),
+  age: z.number({ invalid_type_error: "The age should be a number" }).min(18),
+});
+
+type FormValues = z.infer<typeof schema>;
 
 const UserForm = () => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+      age: 18,
+    },
+  });
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
-    <form>
-      <div className="mb-3 flex flex-col gap-2">
-        <Label htmlFor="name" className="mb-3">
-          Name
-        </Label>
-        <Input type="text" id="name" />
-      </div>
-      <div className="mb-3 flex flex-col gap-2">
-        <Label htmlFor="age" className="mb-3">
-          Age
-        </Label>
-        <Input type="number" id="name" />
-      </div>
-      <Button type="submit">Submit</Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="name">Name</FormLabel>
+              <FormControl>
+                <Input type="text" id="name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="age"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="age" className="mb-3">
+                Age
+              </FormLabel>
+              <FormControl>
+                <Input type="number" id="name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="age"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor="age" className="mb-3">
+                Age
+              </FormLabel>
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value.toString()}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a verified email to display" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="m@example.com">m@example.com</SelectItem>
+                    <SelectItem value="m@google.com">m@google.com</SelectItem>
+                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={!form.formState.isValid}>
+          Submit
+        </Button>
+      </form>
+    </Form>
   );
 };
 
