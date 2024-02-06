ad5bcfed07c92f597737720a0364d16a0f3064e1
Mark Lester Echavez <marklester.echavez@gmail.com>
2024-02-06 11:31:41 +0800

    feat: setup react testing library

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   testEnvironment: "jsdom",
   verbose: true,
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
